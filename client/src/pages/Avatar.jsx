03b43ad449889a05339f5e857a54f4138551fe98
Scooter2727 <scottgmclaughlin@icloud.com>
2026-03-19 17:46:49 -0400

    fixed some package errors

--- client/src/pages/Avatar.jsx
+++ client/src/pages/Avatar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import multiavatar from '@multiavatar/multiavatar/esm';
+import multiavatar from '@multiavatar/multiavatar';
 import '../App.css';
 
 const clothingOptions = [
